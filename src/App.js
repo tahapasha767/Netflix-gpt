36192eb0b92ed3b048107bb487c8ce5e6ef2e86c
Taha <tahapasha121@gmail.com>
2024-04-02 00:34:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='bg-gray-800 w-40 h-40 rounded-lg flex justify-center 
+    items-center'> 
+    <div className=' w-20 h-20 bg-gray-700 rounded-xl shadow-xl shadow-yellow-400 flex justify-center items-center'>
+      <div className='text-5xl font-bold text-yellow-400 '> X</div>
+      
+    </div>
+    </div>
+  );
+}
+
+export default App;
