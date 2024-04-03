f91082d05e117db8ef456829bac523dbe52ac42c
Taha <tahapasha121@gmail.com>
2024-04-03 19:34:18 +0530

    Sign in Sign up toggeling and yeah thats all

--- src/componenets/Login.js
+++ src/componenets/Login.js
@@ -1,7 +1,13 @@
+import { useState } from "react"
 import Header from "./Header"
 import Signin from "./Signin"
 
 export const Login = () => {
+    const[signin,setsignin]=useState(true);
+    function toggelsignin(){
+        setsignin(!signin);
+
+    }
     return (
         <div className="relative">
             <Header />
@@ -11,12 +17,16 @@ export const Login = () => {
                 
             </div>
             <div className="flex justify-center">
-            <form className="absolute w-[450px] h-[500px] bg-black mt-28 rounded-xl flex flex-col items-center gap-6 pt-14 bg-opacity-75">
-               <div className="text-white text-4xl font-bold text-left opacity"><div className="">Sign In</div></div> 
+            <form className="absolute w-3/7 scale-[0.85] sm:absolute w-[450px] h-[500px] bg-black mt-28 rounded-xl flex flex-col items-center gap-6 pt-14 bg-opacity-75 scale-100">
+               <div className="text-white text-4xl font-bold text-left opacity"><div className="">{signin?"Sign In":"Sign up"}</div></div> 
+               {(!signin)&&<input className="p-3 pr-36 rounded-lg bg-gray-900 border-2 bg-opacity-70 text-white" type="text" placeholder="Full name"></input>}
                <input className="p-3 pr-36 rounded-lg bg-gray-900 border-2 bg-opacity-70 text-white" type="text" placeholder="email"></input>
                <input className="p-3 pr-36 rounded-lg bg-black border-2 bg-opacity-70 text-white" type="text" placeholder="password"></input>
-               <button className="p-3 px-36 bg-red-600 rounded-lg text-white font-bold ">Sign In</button>
-               <p className="text-white">New to NetflixGPT, Click here</p>
+               <button className="p-3 px-36 bg-red-600 rounded-lg text-white font-bold ">{signin?"Sign In":"Sign up"}</button>
+               <p className="text-white cursor-pointer" onClick={()=>{
+                toggelsignin();
+               }}>{signin?"New to NetflixGPT, Click here":"Already a user ? Sign In"}</p>
+               
 
             </form>
             </div>
